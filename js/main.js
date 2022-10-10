9e653af59d0f009e9dfa1b8869f99fdb05c2e531
Kevin Lee <sushi@Kevins-MacBook-Air-487.local>
2022-10-10 15:47:32 -0700

    polished css

---
+++ js/main.js
@@ -0,0 +1,24 @@
+/*----- constants -----*/
+const wordBank = [
+    'moon',
+    'space',
+    'stars',
+    'sun',
+    'nova'
+
+];
+
+const maxWrongGuess = 5;
+
+  /*----- state variables -----*/
+let numGuesses
+let numCorrectGuesses
+let numWrongguesses
+
+  /*----- cached elements  -----*/
+
+
+  /*----- event listeners -----*/
+
+
+  /*----- functions -----*/
